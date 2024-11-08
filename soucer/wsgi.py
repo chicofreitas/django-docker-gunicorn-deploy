75c9e5cf6346cb54a46b650670eb33eeb995e429
Chico Freitas <chicoo.freitas84@gmail.com>
2024-11-08 08:10:31 -0300

    fix(statics): add static folder do gitignore.

--- soucer/wsgi.py
+++ soucer/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for launcher project.
+WSGI config for soucer project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'launcher.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'soucer.settings')
 
 application = get_wsgi_application()
